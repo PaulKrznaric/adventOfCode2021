a22230f38efbd16acd24c25c72d5db6a5548f4c9
PaulKrznaric <paulkrznaric@gmail.com>
2021-12-17 22:57:38 -0500

    Added Logic for Part 1

--- day2.py
+++ day2.py
@@ -1,2 +1,32 @@
+from reusablemethods import readStringFile
+
+
 def day2():
-    return "Hi"
+    directions = readStringFile("day2.txt")
+    x = 0
+    z = 0
+    for dir in directions:
+        direction = dir.split()
+        if direction.__getitem__(0) == 'forward':
+            x = moveForward(x, int(direction.__getitem__(1)))
+        elif direction[0] == 'down':
+            z = moveDown(z, int(direction.__getitem__(1)))
+        elif direction[0] == 'up':
+            z = moveUp(z, int(direction.__getitem__(1)))
+        else:
+            raise Exception("Input is invalid. Double check what you're doing ya dunce.")
+    return [x, z]
+
+
+def moveForward(x, distance):
+    return x + distance
+
+
+def moveDown(z, distance):
+    if z + distance < 0:
+        raise Exception("Can't go below 0")
+    return distance + z
+
+
+def moveUp(z, distance):
+    return z - distance
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from day2 import day2
 
 
-print(day2())
+day2_vals = day2()
+print(day2_vals[0] * day2_vals[1])
