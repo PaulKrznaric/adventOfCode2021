dbe727b446a3382b91ac6ca713d103bb311f7d03
PaulKrznaric <paulkrznaric@gmail.com>
2021-12-18 12:14:25 -0500

    Renamed methods in Day 2 for python consistency

--- day2.py
+++ day2.py
@@ -8,27 +8,27 @@ def day2_part1():
     for dir in directions:
         direction = dir.split()
         if direction.__getitem__(0) == 'forward':
-            x = moveForward(x, int(direction.__getitem__(1)))
+            x = move_forward(x, int(direction.__getitem__(1)))
         elif direction[0] == 'down':
-            z = moveDown(z, int(direction.__getitem__(1)))
+            z = move_down(z, int(direction.__getitem__(1)))
         elif direction[0] == 'up':
-            z = moveUp(z, int(direction.__getitem__(1)))
+            z = move_up(z, int(direction.__getitem__(1)))
         else:
             raise Exception("Input is invalid. Double check what you're doing ya dunce.")
     return [x, z]
 
 
-def moveForward(x, distance):
+def move_forward(x, distance):
     return x + distance
 
 
-def moveDown(z, distance):
+def move_down(z, distance):
     if z + distance < 0:
         raise Exception("Can't go below 0")
     return distance + z
 
 
-def moveUp(z, distance):
+def move_up(z, distance):
     return z - distance
 
 
@@ -39,27 +39,27 @@ def day2_part2():
     for direction in directions:
         direction = direction.split()
         if direction.__getitem__(0) == 'forward':
-            coords = moveForward_2(coords, int(direction.__getitem__(1)))
+            coords = move_forward_v2(coords, int(direction.__getitem__(1)))
         elif direction[0] == 'down':
-            coords = moveDown_2(coords, int(direction.__getitem__(1)))
+            coords = move_down_v2(coords, int(direction.__getitem__(1)))
         elif direction[0] == 'up':
-            coords = moveUp_2(coords, int(direction.__getitem__(1)))
+            coords = move_up_v2(coords, int(direction.__getitem__(1)))
         else:
             raise Exception("Input is invalid. Double check what you're doing ya dunce.")
     return coords
 
 
-def moveForward_2(coords, distance):
+def move_forward_v2(coords, distance):
     coords[0] = coords[0] + distance
     coords[1] = coords[1] + (coords[2] * distance)
     return coords
 
 
-def moveDown_2(coords, distance):
+def move_down_v2(coords, distance):
     coords[2] = coords[2] + distance
     return coords
 
 
-def moveUp_2(coords, distance):
+def move_up_v2(coords, distance):
     coords[2] = coords[2] - distance
     return coords
